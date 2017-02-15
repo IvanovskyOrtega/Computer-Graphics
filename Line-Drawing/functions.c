c5979ed72738aafcfc031530f0e4fa0ae4f7b0a8
Ivanovsky <ivanovskyortega@gmail.com>
2017-02-15 17:53:34 -0600

    Functions library

--- Line-Drawing/functions.c
+++ Line-Drawing/functions.c
@@ -40,34 +40,34 @@ void lineProgram()
 	scanf("%lf",&x1);
 	printf("y1:	");	
 	scanf("%lf",&y1);
-	slope = (y1 - y0)/(x1 - x0);
+	slope = ( y1 - y0 )/( x1 - x0 );
 	b = y0 - (slope*x0);
 	printf("The line ecuation is: y = %lfx + %lf\n",slope,b);
 	fillRaster(x0,y0,x1,y1,slope,b,Raster);
 	drawLine(Raster,width,height);
 }
 
-void fillRaster(double x0, double y0, double x1, double y1, double slope, double b, int **Raster)
+void fillRaster( double x0, double y0, double x1, double y1, double slope, double b, int **Raster )
 {
-	putPixel(x0,y0,Raster);
-	for(int i = x0 + 1 ; i <= x1 ; i++)
+	putPixel( x0 , y0 , Raster );
+	for( int i = x0 + 1 ; i <= x1 ; i++ )
 	{
 		y0 = y0 + slope;
-		putPixel(i,(int)y0,Raster);
+		putPixel( i , (int)y0 , Raster );
 	}
 }
 
-void putPixel(int x, int y, int **Raster)
+void putPixel( int x, int y, int **Raster )
 {
 	Raster[x][y]=1;
 }
-void drawLine(int **Raster, int width, int height)
+void drawLine( int **Raster, int width, int height )
 {
-	for(int i = height - 1 ; i >=0 ; i--)
+	for( int i = height - 1 ; i >=0 ; i-- )
 	{
-		for(int j = 0 ; j < width ; j++)
+		for( int j = 0 ; j < width ; j++ )
 		{
-			if(Raster[j][i]==0){
+			if(Raster[ j ][ i ]==0){
 				printf("| |");			
 			}
 			else
--- Line-Drawing/functions.h
+++ Line-Drawing/functions.h
@@ -2,8 +2,8 @@
 #define _FUNCTIONS_H_
 
 void lineProgram();
-void fillRaster(double x0, double y0, double x1, double y1, double slope, double b, int **Raster);
-void putPixel(int x, int y, int **Raster);
-void drawLine(int **Raster, int width, int height);
+void fillRaster( double x0, double y0, double x1, double y1, double slope, double b, int **Raster );
+void putPixel( int x, int y, int **Raster );
+void drawLine( int **Raster, int width, int height );
 
 #endif
