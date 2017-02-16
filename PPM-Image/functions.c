bec3453dc84b6a84e21601973ed6dfb6e5ff3a85
Ivanovsky <ivanovskyortega@gmail.com>
2017-02-15 18:08:16 -0600

    Functions library

---
+++ PPM-Image/functions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "functions.h"
+
+void generateFlag()
+{
+	printf("P3\n");
+	printf("1920 1080\n");
+	printf("255\n");
+	for( i = 0 ; i < 1080 ; i++ )
+	{
+		for( j = 0 ; j < 1920 ; j++ )
+		{
+			if( j < 640 )
+				printf(" 0 255 0 ");
+			else if (j >= 640 && j < 1280 && i < 360)
+				printf(" 255 255 255 ");
+			else if (j >= 640 && j < 1280 && i >= 720)
+				printf(" 255 255 255 ");
+			else if (j >= 640 && j < 1280 && i >= 360 && i < 720 )
+				printf(" 156 102  31 ");
+			else if( j >= 1280 )
+				printf(" 255 0 0 ");
+		}
+		printf("\n");
+	}	
+}
+
---
+++ PPM-Image/functions.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_
+
+void generateFlag();
+
+#endif
