444bb76bb0fe2dab89edb2185c6624600cd6f237
Ivanovsky <ivanovskyortega@gmail.com>
2017-02-21 19:53:28 -0600

    New functions have been added

--- Line-Drawing/functions.h
+++ Line-Drawing/functions.h
@@ -2,8 +2,16 @@
 #define _FUNCTIONS_H_
 
 void lineProgram();
-void fillRaster( double x0, double y0, double x1, double y1, double slope, double b, int **Raster );
+int** createRaster( int** Raster, int width, int height );
+int** emptyRaster( int** Raster, int width, int height );
+double* getXYValues( double* Points );
+int evaluateSlope( double slope, double* Points );
+void fillSpecialCase1( double x0, double y0, double x1, double y1, int **Raster );
+void fillSpecialCase2( double x0, double y0, double x1, double y1, int **Raster );	
+void fillRasterDDA( double x0, double y0, double x1, double y1, double slope, int **Raster, int evaluatedSlope );
+void fillRasterNaive( double x0, double y0, double x1, double y1, double slope, double b, int **Raster, int evaluatedSlope );
+void fillRasterBresenham( int x0, int y0, int x1, int y1, int **Raster, int evaluatedSlope );
 void putPixel( int x, int y, int **Raster );
-void drawLine( int **Raster, int width, int height );
+void drawLine( int **Raster, int width, int height, int evaluatedSlope );
 
 #endif
