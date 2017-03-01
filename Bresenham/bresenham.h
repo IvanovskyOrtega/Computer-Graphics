e670bf3cacca77d6899626a7f60dde1f0d261841
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-02-28 22:39:48 -0600

    bresenham.h

--- Bresenham/bresenham.h
+++ Bresenham/bresenham.h
@@ -10,6 +10,6 @@ void fillSpecialCase3( double x0, double y0, double x1, double y1, unsigned char
 void fillSpecialCase4( double x0, double y0, double x1, double y1, unsigned char*** Raster, unsigned char* rgb );	
 void fillRasterBresenham( int x0, int y0, int x1, int y1, unsigned char*** Raster, int evaluatedSlope, unsigned char* rgb );
 void putPixel( int x, int y, unsigned char*** Raster, unsigned char* rgb );
-void drawLine( unsigned char*** Raster, int width, int height, int evaluatedSlope );
+void drawLine( unsigned char*** Raster, int width, int height );
 
 #endif
