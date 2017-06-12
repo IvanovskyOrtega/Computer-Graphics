e9a5d4f3268c42b8dabd3f4bc714431ebbe32f73
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-06-12 18:13:18 -0500

    Removed unused functions

--- Face-Hidding-ZBuffer/vertex.h
+++ Face-Hidding-ZBuffer/vertex.h
@@ -3,15 +3,13 @@
 
 double **createZBuffer ();
 void VLF (double alfa, double beta, double gamma, double sfX, double sfY,
-	  double tX, double tY, char *name);
-int readLines ();
-void vertexList (struct vertex *vertexes, int n);
-void vertexList2 (struct vertex *vertexes, int n);
-void edgesList (struct edge *edges, struct vertex *vertexes, int n);
+	  double tX, double tY, char *name, char *rawFile);
+int readLines (char *rawFile);
+void vertexList (struct vertex *vertexes, int n, char *rawFile);
+void vertexList2 (struct vertex *vertexes, int n, char *rawFile);
+void edgesList (struct edge *edges, struct vertex *vertexes, int n,
+		char *rawFile);
 void facesList (struct face **faces, struct vertex *vertexes,
-		struct edge *edges, int n);
-int numberOfVertexes (struct vertex *vertexes, int n);
-int numberOfEdges (struct edge *edges, int n);
-int numberOfFaces (struct face *faces, int n);
+		struct edge *edges, int n, char *rawFile);
 
 #endif
