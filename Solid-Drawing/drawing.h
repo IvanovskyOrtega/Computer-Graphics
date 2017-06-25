93f825e68ca9e65d290735a35fd048ba45507c73
Ivanovsky <ivanovskyortega@gmail.com>
2017-06-24 21:25:41 -0500

    drawing.h

---
+++ Solid-Drawing/drawing.h
@@ -0,0 +1,11 @@
+#ifndef _DRAWING_H_
+#define _DRAWING_H_
+
+#define PI 3.14159265
+
+double faceHidding(struct face f, struct vertex *vertexes, struct edge *edges);
+void scanline(struct pixels ***Raster, double **ZBuffer, unsigned char *rgb,
+	      struct vertex normal, struct equation planeEquation);
+double min(double a);
+unsigned char max(double a);
+#endif
