40b9b43d4de66dfa050d13cd4e0455c547fbcf80
Ivanovsky <ivanovskyortega@gmail.com>
2017-06-24 21:28:03 -0500

    Using the plane equation didn't worked as I hoped :(

---
+++ Solid-Drawing/main.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+#include"bresenham.h"
+#include"vertex.h"
+
+/**
+  * Author: Ortega Victoriano Ivan
+  * Subject: Computer Graphics
+  * Group: 3CM6
+  * Check my CG Github reppository:
+  * https://github.com/IvanovskyOrtega/Computer-Graphics
+  * Compilation instructions:
+  * Linux:
+  *   $ make      #to compile
+  *   $ make run  #to compile and run (will tell you what arguments are needed)
+  */
+
+/* Just run it if you don't know what parameters are needed */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 12) {
+		double alfa = atof(argv[1]);
+		double beta = atof(argv[2]);
+		double gamma = atof(argv[3]);
+		double sfX = atof(argv[4]);
+		double sfY = atof(argv[5]);
+		double sfZ = atof(argv[6]);
+		double tX = atof(argv[7]);
+		double tY = atof(argv[8]);
+		double tZ = atof(argv[9]);
+		VLF(alfa, beta, gamma, sfX, sfY, sfZ, tX, tY, tZ, argv[10], argv[11]);
+		return 0;
+	} else {
+		printf("Arguments are not enough :C\n");
+		printf("(Or maybe you sent more :V)\n");
+		printf("Try with the following pattern:\n");
+		printf
+		    ("./a.out alfa beta gamma sfX sfY sfZ tX tY tZ outputName inputName\n");
+		printf("Where:\n");
+		printf("alfa\t: Rotation angle in the X axis\n");
+		printf("beta\t: Rotation angle in the Y axis\n");
+		printf("gamma\t: Rotation angle in the Z axis\n");
+		printf("sfX\t: X axis scale factor\n");
+		printf("sfY\t: Y axis scale factor\n");
+		printf("sfZ\t: Z axis scale factor\n");
+		printf("tX\t: Translation in the X axis\n");
+		printf("tY\t: Translation in the Y axis\n");
+		printf("tZ\t: Translation in the Z axis\n");
+		printf
+		    ("outputName\t: The name your ppm image is going to have (Just the name, extension its not necessary)\n");
+		printf
+		    ("inputName\t: The raw file name  (Add the extension Ej.: eva.raw )\n");
+		printf("An example could be:\n");
+		printf
+		    ("./a.out -0.5 0.1 0.2 1200 1200 1200 960 540 0 myppm getintherobotshinji.raw\n");
+	}
+}
